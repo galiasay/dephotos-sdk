8d8c7d1218015117e2125b0c0e2d92fd2e99039d
Andrii Yakymenko <andrii.yakymenko@depositphotos.com>
2021-12-30 19:04:30 +0200

    Fix unit tests

--- tests/Resource/ResourceTrait.php
+++ tests/Resource/ResourceTrait.php
@@ -5,6 +5,7 @@ namespace Depositphotos\SDK\Tests\Resource;
 
 use Depositphotos\SDK\Http\HttpClient;
 use GuzzleHttp\Psr7\Utils;
+use Psr\Http\Client\ClientInterface;
 use Psr\Http\Message\RequestInterface;
 use Psr\Http\Message\ResponseInterface;
 
@@ -15,12 +16,12 @@ trait ResourceTrait
         $response = $this->createMock(ResponseInterface::class);
         $response->method('getBody')->willReturn(Utils::streamFor(json_encode($responseData)));
 
-        $httpClient = $this->createMock(HttpClient::class);
-        $httpClient->method('sendRequest')->with($this->callback(function (RequestInterface $request) use ($requestData) {
+        $client = $this->createMock(ClientInterface::class);
+        $client->method('sendRequest')->with($this->callback(function (RequestInterface $request) use ($requestData) {
             $this->assertEquals((string) $request->getBody(), json_encode($requestData));
            return true;
         }))->willReturn($response);
 
-        return $httpClient;
+        return new HttpClient($client);
     }
 }
