4b6d5ac1fbc7fab6a240041a86deba29430ffd1e
Andrii Yakymenko <andrii.yakymenko@depositphotos.com>
2022-02-07 16:25:20 +0200

    Add user resource for enterprise

--- src/EnterpriseClient.php
+++ src/EnterpriseClient.php
@@ -3,7 +3,12 @@ declare(strict_types=1);
 
 namespace Depositphotos\SDK;
 
+use Depositphotos\SDK\Resource\Enterprise\User\UserResource;
+
 class EnterpriseClient extends Client
 {
-
+    public function user(): UserResource
+    {
+        return new UserResource($this->httpClient);
+    }
 }
