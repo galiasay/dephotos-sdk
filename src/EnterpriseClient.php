afeca4f3f610347e474ed09a623bff6ea08ece01
Andrii Yakymenko <andrii.yakymenko@depositphotos.com>
2022-02-16 13:25:45 +0200

    Add item resource for enterprise

--- src/EnterpriseClient.php
+++ src/EnterpriseClient.php
@@ -3,7 +3,12 @@ declare(strict_types=1);
 
 namespace Depositphotos\SDK;
 
+use Depositphotos\SDK\Resource\Enteprise\Item\ItemResource;
+
 class EnterpriseClient extends Client
 {
-
+    public function item(): ItemResource
+    {
+        return new ItemResource($this->httpClient);
+    }
 }
--- src/Resource/Item/Response/CheckItemsStatusResponse.php
+++ src/Resource/Item/Response/CheckItemsStatusResponse.php
@@ -1,7 +1,7 @@
 <?php
 declare(strict_types=1);
 
-namespace Depositphotos\SDK\Resource\Regular\Item\Response;
+namespace Depositphotos\SDK\Resource\Item\Response;
 
 use Depositphotos\SDK\Resource\ResponseObject;
 
