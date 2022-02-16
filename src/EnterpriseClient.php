65666903a1c6321b6913b0ac031469f17cd12a4c
Andrii Yakymenko <andrii.yakymenko@depositphotos.com>
2022-02-16 13:20:20 +0200

    Add license resource for enterprise

--- src/EnterpriseClient.php
+++ src/EnterpriseClient.php
@@ -3,7 +3,12 @@ declare(strict_types=1);
 
 namespace Depositphotos\SDK;
 
+use Depositphotos\SDK\Resource\Enteprise\License\LicenseResource;
+
 class EnterpriseClient extends Client
 {
-
+    public function license(): LicenseResource
+    {
+        return new LicenseResource($this->httpClient);
+    }
 }
